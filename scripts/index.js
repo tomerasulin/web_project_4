68b0deb07df8e83255ff692893a00ac6dfa25605
TomerAsulin <tomermailg@gmail.com>
2022-01-09 00:34:28 +0200

    fix project

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,9 @@
 const popupBox = document.querySelector(".popup-box");
-const openPopup = document.querySelector(".profile-info__edit-btn");
+const openPopup = document.querySelector(".profile__edit-btn");
 const closePopup = document.querySelector(".popup-box__close-btn");
-const saveBtn = document.querySelector(".popup-box__save-btn");
 const likeBtn = document.querySelectorAll(".element__like-btn");
-const profileName = document.querySelector(".profile-info__name");
-const profileAbout = document.querySelector(".profile-info__role");
+const profileName = document.querySelector(".profile__name");
+const profileAbout = document.querySelector(".profile__role");
 const userInputName = document.querySelector(".popup-box__input_type_name");
 const userInputAbout = document.querySelector(".popup-box__input_type_about");
 
@@ -20,14 +19,6 @@ function handleEditButton(){
 
 openPopup.addEventListener("click", handleEditButton);
 closePopup.addEventListener("click", togglePopup);
-
-function activateLikeButton(ind) {
-  if (likeBtn[ind].getAttribute("src") === "./images/Like Logo.svg") {
-    likeBtn[ind].setAttribute("src", "./images/ActiveLike.svg");
-  } else {
-    likeBtn[ind].setAttribute("src", "./images/Like Logo.svg");
-  }
-}
 
 const formElement = document.querySelector(".popup-box__form");
 function handleProfileFormSubmit(evt) {
