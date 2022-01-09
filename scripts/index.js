cb02cb4d229ad19d1bf50c5712e34878b60bd568
TomerAsulin <tomermailg@gmail.com>
2022-01-09 17:35:24 +0200

    fix popup window pos && minor html issues

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// declaring variables
 const popupBox = document.querySelector(".popup-box");
 const openPopup = document.querySelector(".profile__edit-btn");
 const closePopup = document.querySelector(".popup-box__close-btn");
@@ -6,21 +7,21 @@ const profileName = document.querySelector(".profile__name");
 const profileAbout = document.querySelector(".profile__role");
 const userInputName = document.querySelector(".popup-box__input_type_name");
 const userInputAbout = document.querySelector(".popup-box__input_type_about");
+const formElement = document.querySelector(".popup-box__form");
 
+// function that handle the popup window
 function togglePopup() {
   popupBox.classList.toggle("popup-box_opened");
 }
 
+// function that handle the edit button while a user press it
 function handleEditButton(){
   userInputName.value = profileName.textContent;
   userInputAbout.value = profileAbout.textContent;
   togglePopup();
 }
 
-openPopup.addEventListener("click", handleEditButton);
-closePopup.addEventListener("click", togglePopup);
-
-const formElement = document.querySelector(".popup-box__form");
+// this function handle the save buttom while the user enter input
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = userInputName.value;
@@ -28,5 +29,8 @@ function handleProfileFormSubmit(evt) {
   togglePopup();
 }
 
+// event listeners 
+openPopup.addEventListener("click", handleEditButton);
+closePopup.addEventListener("click", togglePopup);
 formElement.addEventListener("submit", handleProfileFormSubmit);
 
